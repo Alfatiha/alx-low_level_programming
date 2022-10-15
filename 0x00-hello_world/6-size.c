67ed9ce36f5aabbdcbf4425ae04230340c0f614e
Alfatiha <alfatihaahmed@gmail.com>
2022-10-15 08:48:09 -0700

    some fixes for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int age;
 long int age_of;
 long long int age_for;
 float number;
-	printf("size of a char: %zu byte(s)", sizeof(character));
-	printf("\nsize of an int: %zu byte(s)", sizeof(age));
-	printf("\nsize of a long int: %zu byte(s)", sizeof(age_of));
-	printf("\nsize of a long long int: %zu byte(s)", sizeof(age_for));
-	printf("\nsize of a float: %zu byte(s)\n", sizeof(number));
+	printf("size of a char: %lu byte(s)", sizeof(character));
+	printf("\nsize of an int: %lu byte(s)", sizeof(age));
+	printf("\nsize of a long int: %lu byte(s)", sizeof(age_of));
+	printf("\nsize of a long long int: %lu byte(s)", sizeof(age_for));
+	printf("\nsize of a float: %lu byte(s)\n", sizeof(number));
 	return (0);
 }
