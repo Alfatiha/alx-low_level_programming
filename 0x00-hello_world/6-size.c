bcdc3376b555ade34ac8fc618f4c5fc6fc44093b
Alfatiha <alfatihaahmed@gmail.com>
2022-10-15 10:22:53 -0700

    i hope this is the final fix for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main - the size of various types on the computer
+ * main - print out sizes of data types in C
  * Return: 0
 */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-	printf("size of a char: %lu byte(s)", (unsigned long) sizeof(a));
-	printf("\nsize of an int: %lu byte(s)", (unsigned long) sizeof(b));
-	printf("\nsize of a long int: %lu byte(s)", (unsigned long)sizeof(c));
-	printf("\nsize of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
-	printf("\nsize of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
