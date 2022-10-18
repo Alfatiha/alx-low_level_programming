19abe78887dd739ce7136acab32fd47d57fda0ab
Alfatiha <alfatihaahmed@gmail.com>
2022-10-17 18:34:16 -0700

    added for the task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print nums from zero to ten
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
