e473170eb85a329ba3be20ee00baf2af04eb0b3f
Alfatiha <alfatihaahmed@gmail.com>
2022-10-20 12:13:12 -0700

    arranged files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a sentence
+ * result: 1
+ */
+int main(void)
+
+{
+	fwrite(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+
+}
