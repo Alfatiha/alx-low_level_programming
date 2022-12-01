6fe4dc09460bf0eb973d4599e705bb70d3680147
Alfatiha <alfatihaahmed@gmail.com>
2022-12-01 06:48:39 +0000

    lo

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the list
+ *
+ * @head: the head of list
+ *
+ * Return: NULL if error
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
